dd42856ab7cc0e00495b5246ff4ff6218aed42ea
Havish <hpatel84@uwo.ca>
2016-02-20 02:03:03 -0500

    Temperature Converter

--- MyTempConverter/app/src/main/java/app/tempconverter/com/mytempconverter/MainActivity.java
+++ MyTempConverter/app/src/main/java/app/tempconverter/com/mytempconverter/MainActivity.java
@@ -8,8 +8,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText myTemp;
+    private Button celButton;
+    private Button fButton;
+    private TextView myTextView;
+
+    DecimalFormat round = new DecimalFormat("0.0");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +39,67 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        myTemp = (EditText) findViewById(R.id.editTextId);
+        celButton = (Button) findViewById(R.id.buttonCId);
+        fButton =(Button) findViewById(R.id.buttonFId);
+        myTextView = (TextView) findViewById(R.id.textViewId);
+
+        celButton.setOnClickListener(new View.OnClickListener(){ //F to C
+            @Override
+            public void onClick(View v) {
+                String holder= myTemp.getText().toString(); //get value from user
+
+                if (holder.isEmpty()){ //checking if empty
+                    Toast.makeText(getApplicationContext(),"Enter a Value", Toast.LENGTH_LONG).show();
+
+                }else{
+                    double tempVal = Double.parseDouble(holder); //making holder an int value
+                    double CVal = ConvertToC(tempVal); //getting F value
+
+                    String result = String.valueOf(round.format(CVal)) + " C";
+                    myTextView.setText(result);
+
+                }
+
+            }
+        });
+
+
+        fButton.setOnClickListener(new View.OnClickListener() { //C to F
+            @Override
+            public void onClick(View v) {
+                String holder= myTemp.getText().toString(); //get value
+
+                if (holder.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Enter a Value", Toast.LENGTH_LONG).show();
+
+                }else{
+                    double tempVal = Double.parseDouble(holder); //convert to int
+                    double FVal = ConvertToF(tempVal); //send to be converted
+
+                    String result = String.valueOf(round.format(FVal)) + " F";
+                    myTextView.setText(result);
+
+                }
+
+            }
+        });
+
+    }
+
+    public double ConvertToC (double farVal){ //converting to Celsius
+
+        double cResult;
+        cResult = (farVal -32) * 5/9;
+        return cResult;
+    }
+
+    public double ConvertToF (double cVal){ //converting to Fahrenheit
+
+        double fResult;
+        fResult = (cVal *9/5) +32;
+        return fResult;
     }
 
     @Override
